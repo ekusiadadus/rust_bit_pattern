ae9cab28cf2bb25a09d599b5dd54602a3eed328d
ekusiadadus <ekusiadadus@gmail.com>
2022-02-15 14:40:53 +0900

    Check Type Change

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-    let a: u16 = 50115;
-    let b: i16 = -15421;
-    println!("a: {:016b} {}", a,a);
-    println!("a: {:016b} {}", b,b);
+    let a: f32 = 42.42;
+    let frankentype: u32 = unsafe{
+        std::mem::transmute(a)
+    };
+
+    println!("{:?}", frankentype);
+    println!("{:032b}",frankentype);
+
+    let b: f32 = unsafe{
+        std::mem::transmute(frankentype)
+    };
+    println!("{}",b);
+    assert_eq!(a,b);
 }
